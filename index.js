c0664a858d0239232d5b6d6053eed810c4eedb0f
mcfinley <mcfinleyandrew@gmail.com>
2018-01-22 11:10:05 +0300

    Finish refactoring

--- index.js
+++ index.js
@@ -51,6 +51,30 @@ const rules = [
     },
   },
 
+  // DIGIT MUL DIGIT
+  {
+    match: (state, token, next) => {
+      const v = state ? state.v : 0;
+
+      if (typeof(token) !== 'object') {
+        return { match: 'none', v: 0 };
+      } else {
+        if (token.type === 'expression' && v === 0 && next) {
+          return { match: 'part', v: 1 };
+        } else if (token.type === 'mul' && v === 1 && next) {
+          return { match: 'part', v: 2 };
+        } else if (token.type === 'expression' && v === 2) {
+          return { match: 'full', v: 3 };
+        } else {
+          return { match: 'none', v: 0 };
+        }
+      }
+    },
+    wrap: (tape, index) => {
+      tape.replace(index - 2, 3, { type: 'expression', $sub: 'mul' } )
+    },
+  },
+
   // DIGIT PLUS DIGIT
   {
     match: (state, token, next) => {
@@ -75,29 +99,7 @@ const rules = [
     },
   },
 
-  // DIGIT MUL DIGIT
-  {
-    match: (state, token, next) => {
-      const v = state ? state.v : 0;
 
-      if (typeof(token) !== 'object') {
-        return { match: 'none', v: 0 };
-      } else {
-        if (token.type === 'expression' && v === 0 && next) {
-          return { match: 'part', v: 1 };
-        } else if (token.type === 'mul' && v === 1 && next) {
-          return { match: 'part', v: 2 };
-        } else if (token.type === 'expression' && v === 2) {
-          return { match: 'full', v: 3 };
-        } else {
-          return { match: 'none', v: 0 };
-        }
-      }
-    },
-    wrap: (tape, index) => {
-      tape.replace(index - 2, 3, { type: 'expression', $sub: 'mul' } )
-    },
-  },
 ];
 
 const parser = new StreamParser(rules);
