89da62ac1521903a9b6e4a413ef7e08dd471427b
mcfinley <mcfinleyandrew@gmail.com>
2018-02-06 22:24:06 +0300

    It works!

--- index.js
+++ index.js
@@ -2,38 +2,69 @@ const State = require('./lib/State');
 const Grammar = require('./lib/Grammar');
 const Rule = require('./lib/Rule');
 const Situation = require('./lib/Situation');
-
+const { find, equal } = require('./lib/helpers');
 // Create a Grammar and add rules
 
 const grammar = new Grammar("E");
 
-// grammar.push(new Rule("E", ["E", "P", "E"]))
-grammar.push(new Rule("E", ["A", "A"], 'test'))
-grammar.push(new Rule("A", ["B", "C"], 'test2'))
+grammar.push(new Rule("E", ["E", "M", "E"], 'mul'))
+grammar.push(new Rule("E", ["E", "P", "E"], 'sum'))
+grammar.push(new Rule("E", ["N"], 'ez'))
 
-// Create new state
+/* */
+
+const hasOccured = (states, state) => {
+  let result = -1;
+
+  states.forEach((_state, i) => {
+    _state.situations.forEach((situation) => {
+      state.situations.forEach((_situation) => {
+        if (equal(_situation, situation)) {
+          result = i;
+        }
+      });
+    });
+  });
+
+  return result;
+};
+
+const states = [];
 
 const state = new State(new Situation(grammar.getBaseRule()));
 
-const strtabs = (tabs) => new Array(tabs).fill('.').join('');
+states.push(state);
 
-const test = (state, tabs = 0) => {
+while (states.filter(state => !state.$parsed).length > 0) {
+  const i = find(states, state => !state.$parsed);
+  const state = states[i];
+
   state.expand(grammar);
-  const nextTokens = state.getNextTokens();
 
-  Object.keys(nextTokens).forEach(token => {
-    console.log(strtabs(tabs), 'on token', token);
+  console.log()
+  console.log('state #', i)
 
-    const newState = state.extract(token);
+  const { shift, reduce } = state.getNextTokens();
 
-    if (newState.getReduce().length > 0) {
-      console.log(strtabs(tabs), 'reduce', newState.getReduce()[0].getNextToken().$reduce);
+  Object.keys(reduce).forEach(token => {
+    console.log('on', token, 'reduce', reduce[token]);
+  });
+
+  Object.keys(shift).forEach(token => {
+    console.log('on', token, 'shift');
+
+    const newState = state.filter(token).step();
+    const a = hasOccured(states, newState);
+
+    if (a === -1) {
+      states.push(newState);
+      console.log('to', states.length - 1);
     } else {
-      console.log(strtabs(tabs), 'shift');
-      newState.step();
-      test(newState, tabs + 2);
+      console.log('to', a)
     }
   });
-};
 
-test(state, 0);
+  state.$parsed = true;
+
+  console.log()
+}
