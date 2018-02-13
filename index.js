f5eba5f458345c01aaee4833b09222576aca9d9d
mcfinley <mcfinleyandrew@gmail.com>
2018-02-13 11:00:49 +0300

    trash

--- index.js
+++ index.js
@@ -2,69 +2,57 @@ const State = require('./lib/State');
 const Grammar = require('./lib/Grammar');
 const Rule = require('./lib/Rule');
 const Situation = require('./lib/Situation');
-const { find, equal } = require('./lib/helpers');
+const StatesManager = require('./lib/StatesManager');
 // Create a Grammar and add rules
 
 const grammar = new Grammar("E");
 
-grammar.push(new Rule("E", ["E", "M", "E"], 'mul'))
-grammar.push(new Rule("E", ["E", "P", "E"], 'sum'))
-grammar.push(new Rule("E", ["N"], 'ez'))
+grammar.push(new Rule("E", ["E", "M", "E"], 'mul'));
+grammar.push(new Rule("E", ["E", "P", "E"], 'sum'));
+grammar.push(new Rule("E", ["N"], 'ez'));
 
 /* */
 
-const hasOccured = (states, state) => {
-  let result = -1;
+const statesManager = new StatesManager();
 
-  states.forEach((_state, i) => {
-    _state.situations.forEach((situation) => {
-      state.situations.forEach((_situation) => {
-        if (equal(_situation, situation)) {
-          result = i;
-        }
-      });
-    });
-  });
+statesManager.push(new State(new Situation(grammar.getBaseRule())));
 
-  return result;
-};
-
-const states = [];
-
-const state = new State(new Situation(grammar.getBaseRule()));
-
-states.push(state);
-
-while (states.filter(state => !state.$parsed).length > 0) {
-  const i = find(states, state => !state.$parsed);
-  const state = states[i];
+while (statesManager.hasUnparsed()) {
+  const i = statesManager.getUnparsed();
+  const state = statesManager.get(i);
 
   state.expand(grammar);
 
-  console.log()
-  console.log('state #', i)
+  console.log();
+  console.log('state #', i);
 
-  const { shift, reduce } = state.getNextTokens();
+  const tokens = state.getNextTokens();
 
-  Object.keys(reduce).forEach(token => {
-    console.log('on', token, 'reduce', reduce[token]);
-  });
+  Object.keys(tokens).forEach(token => {
+    const usages = tokens[token];
 
-  Object.keys(shift).forEach(token => {
-    console.log('on', token, 'shift');
+    usages.forEach(usage => {
+      if (usage.reduce) {
+        console.log('on', token, 'reduce', usage.reduce);
+      }
 
-    const newState = state.filter(token).step();
-    const a = hasOccured(states, newState);
+      if (usage.shift) {
+        console.log('on', token, 'shift');
 
-    if (a === -1) {
-      states.push(newState);
-      console.log('to', states.length - 1);
-    } else {
-      console.log('to', a)
-    }
+        const newState = state.filter(token).step();
+        const a = statesManager.getSame(newState);
+
+        if (a === -1) {
+          statesManager.push(newState);
+          console.log('to', statesManager.count() - 1);
+        } else {
+          console.log('to', a)
+        }
+      }
+    });
   });
 
   state.$parsed = true;
 
-  console.log()
+  console.log();
 }
